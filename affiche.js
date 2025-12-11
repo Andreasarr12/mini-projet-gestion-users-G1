a12c69ebfab3abe2b332af7593c3a94dafdbee87
Andreasarr12 <andreamariesarr12@gmail.com>
2025-12-11 19:52:00 +0000

    Update affiche.js

--- affiche.js
+++ affiche.js
@@ -2,4 +2,5 @@ function affiche(){
 alert( "merci pour la collabooration" );
 alert("c'est ma contribution yanni");
 alert ("moi aussi j'ai également contribué en modifiant des fichiers")
+alert("Meucii beaukou")
 }
